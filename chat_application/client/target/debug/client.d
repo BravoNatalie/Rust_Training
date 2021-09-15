fe6f0189c8c35ff650cafb732b0f33af62aaa8c9
Natalie Bravo <37748828+BravoNatalie@users.noreply.github.com>
2021-09-15 16:10:28 -0300

    Delete chat_application/client/target directory

--- chat_application/client/target/debug/client.d
+++
@@ -1,1 +0,0 @@
-/home/bravo/Documents/GitHub/Rust_Training/chat_application/client/target/debug/client: /home/bravo/Documents/GitHub/Rust_Training/chat_application/client/src/main.rs
--- chat_application/client/target/debug/deps/client-49c5150414fa9510.d
+++
@@ -1,5 +0,0 @@
-/home/bravo/Documents/GitHub/Rust_Training/chat_application/client/target/debug/deps/client-49c5150414fa9510: src/main.rs
-
-/home/bravo/Documents/GitHub/Rust_Training/chat_application/client/target/debug/deps/client-49c5150414fa9510.d: src/main.rs
-
-src/main.rs:
